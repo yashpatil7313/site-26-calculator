ce8e6826bdb02aca9c4beeb4b35299491888f08b
Yash patil <yashrspatil7313@gmail.com>
2026-04-20 18:08:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function press(value) {
+    document.getElementById("display").value += value;
+}
+
+function calculate() {
+    let result = eval(document.getElementById("display").value);
+    document.getElementById("display").value = result;
+}
+
+function clearDisplay() {
+    document.getElementById("display").value = "";
+}
